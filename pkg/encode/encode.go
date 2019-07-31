f0ca03f78b01d7c2051368e3acd4c3f84145c341
marcin witalis <45931826+m00g3n@users.noreply.github.com>
2019-07-31 13:39:45 +0200

    Update pkg/encode/encode.go
    Co-Authored-By: Małgorzata Świeca <mitorajmalgorzata@gmail.com>

--- pkg/encode/encode.go
+++ pkg/encode/encode.go
@@ -12,7 +12,7 @@ func ToJSON(i interface{}, writer io.Writer) error {
 	return json.NewEncoder(writer).Encode(i)
 }
 
-// ToYaml writes an asyncapi document in yaml format into a stream
+// ToYaml writes an AsyncAPI document in the YAML format encoding it into a stream.
 func ToYaml(i interface{}, writer io.Writer) error {
 	return yaml.NewEncoder(writer).Encode(i)
 }
